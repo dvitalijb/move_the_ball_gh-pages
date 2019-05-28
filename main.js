830c5be323e572404a16865ac18b16becb46982b
vitalii <dumavb777@gmail.com>
2019-05-28 22:28:25 +0300

    sol

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 const field = document.getElementById('field');
 const ball = document.getElementById('ball');
-const maxMoveInParentWidth = parseInt(getComputedStyle(field).width, 10);
-const maxMoveInParentHeight = parseInt(getComputedStyle(field).height, 10);
-const borderField = parseInt(getComputedStyle(field).borderWidth, 10);
-const marginBody = parseInt(getComputedStyle(document.body).margin, 10);
-const ballWith = parseInt(getComputedStyle(ball).width, 10);
-const ballHeight = parseInt(getComputedStyle(ball).height, 10);
+const maxMoveInParentWidth = field.offsetWidth;
+const maxMoveInParentHeight = field.offsetHeight;
+const borderField = 10;
+const twoBorderFields = borderField * 2;
+const ballWith = ball.offsetWidth;
+const ballHeight = ball.offsetHeight;
+const halfBallWidth = ballWith / 2;
+const halfBallHeight = ballHeight / 2;
 
 function moveBall(event) {
     const { documentElement } = document;
-    const coordinatesY = event.clientY + documentElement.scrollTop;
-    const coordinatesX = event.clientX + documentElement.scrollLeft;
-    const halfBallWidth = ballWith / 2;
-    const halfBallHeight = ballHeight / 2;
-    const lengthBorderMargin = borderField + marginBody;
+    const coordinateY = event.clientY + documentElement.scrollTop;
+    const coordinateX = event.clientX + documentElement.scrollLeft;
+    let ballStyleTop = coordinateY - borderField - halfBallHeight;
+    let ballStyleLeft = coordinateX - borderField - halfBallWidth;
 
-    ball.style.top = (coordinatesY - halfBallHeight - lengthBorderMargin) + 'px';
-    ball.style.left = (coordinatesX - halfBallWidth - lengthBorderMargin) + 'px';
+    if (coordinateX < borderField + halfBallWidth) {
+        ballStyleLeft = 0;
+    }
+    if (coordinateY < borderField + halfBallHeight) {
+        ballStyleTop = 0;
+    }
+    if (coordinateX > (maxMoveInParentWidth - borderField - halfBallWidth)) {
+        ballStyleLeft = maxMoveInParentWidth - ballWith - twoBorderFields;
+    }
+    if (coordinateY > (maxMoveInParentHeight - borderField - halfBallHeight)) {
+        ballStyleTop = maxMoveInParentHeight - ballHeight - twoBorderFields;
+    }
 
-    if (coordinatesX < (halfBallWidth + lengthBorderMargin)) {
-        ball.style.left = 0 + 'px';
-    }
-    if (coordinatesY < (halfBallHeight + lengthBorderMargin)) {
-        ball.style.top = 0 + 'px';
-    }
-    if (coordinatesX > (maxMoveInParentWidth - halfBallWidth + borderField)) {
-        ball.style.left = (maxMoveInParentWidth - ballWith) + 'px';
-    }
-    if (coordinatesY > (maxMoveInParentHeight - halfBallHeight + borderField)) {
-        ball.style.top = (maxMoveInParentHeight - ballHeight) + 'px';
-    }
+    ball.style.top = `${ballStyleTop}px`;
+    ball.style.left = `${ballStyleLeft}px`;
 }
 
 field.addEventListener('click', moveBall, true);
